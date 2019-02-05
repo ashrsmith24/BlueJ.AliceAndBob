2a5dc626b632aeab1fc66e3f96c0185a26ab18f6
Ashley Renee Smith <ashleyreneesmith@zipcoders-MacBook-Pro-2.local>
2019-02-05 13:48:03 -0500

    return hello when you type Alice or Bob

--- Main.java
+++ Main.java
@@ -1,9 +1,15 @@
 /**
  * Created by iyasuwatts on 10/17/17.
  */
+import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args ){
-
-    }
-}
+        Scanner in = new Scanner(System.in);
+        System.out.println("Enter your name?");
+        String n = in.nextLine();
+       if((n.equals("Alice"))||( n.equals("Bob"))){
+           
+       System.out.println("Hello");
+       }
+    }}
